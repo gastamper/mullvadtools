d663a1973b55451ea1597606bbb385059318206b
Greg Stamper <gastamper@gmail.com>
2021-12-24 11:11:09 -0500

    Tracking of used relays fixed

--- reconnect.sh
+++ reconnect.sh
@@ -4,19 +4,20 @@
 usedlist=$HOME/mullvad.used
 relaylist=$(mullvad relay list | grep wireguard | cut -f1 -d ' ' | tr -d '\t')
 
-if [[ -z $usedlist ]]; then
-	used=$(cat $usedlist)
-else
-	used=""
+if [[ ! -f $usedlist ]]; then
+	touch $usedlist
 fi
 
 for item in $relaylist; do
-	if [[ $usedlist =~ (^|[[:space:]])$item($|[[:space:]]) ]] ; then	
+	if [[ $(cat $usedlist) =~ (^|[[:space:]])$item($|[[:space:]]) ]] ; then	
 		echo "Already used $item"
 	else
 		echo "$item free"
-		echo "$item" >> $usedlist
-		mullvad relay set hostname $item && break || echo "Setting relay failed"
+		echo $item >> $usedlist
+		mullvad relay set hostname $item && exit 0
+		echo "Setting relay failed, exiting"
+		exit 1
 	fi
-	echo "All endpoints already used, exiting"
 done
+echo "All endpoints already used, exiting"
+exit 1
