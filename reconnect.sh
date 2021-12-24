d8305eabc4cb545033f15737d83a53e074d28d53
Greg Stamper <gastamper@gmail.com>
2021-12-24 11:49:58 -0500

    Added support for excluding specific countries

--- reconnect.sh
+++ reconnect.sh
@@ -3,6 +3,9 @@
 
 usedlist=$HOME/mullvad.used
 relaylist=$(mullvad relay list | grep wireguard | cut -f1 -d ' ' | tr -d '\t')
+# Get list of countries by 2-character identifier
+# countries=$(mullvad relay list | sed '/^\t/d' | sed '/^$/d' | cut -f2 -d '(' | cut -f1 -d')' | tr '\n' '|')
+exclude="al|au|at|be|br|bg|cz|dk|ee|fi|fr|de|gr|hk|hu|ie|il|it|jp|lv|lu|md|nl|nz|mk|no|pl|pt|ro|rs|sg|es|se|ch|ae"
 
 if [[ ! -f $usedlist ]]; then
 	touch $usedlist
@@ -11,6 +14,9 @@ fi
 for item in $relaylist; do
 	if [[ $(cat $usedlist) =~ (^|[[:space:]])$item($|[[:space:]]) ]] ; then	
 		echo "Already used $item"
+	elif [[ $(echo $exclude | grep ${item::2}) ]] ; then
+		echo "Excluded $item based on country"
+		continue
 	else
 		echo "$item free"
 		echo $item >> $usedlist
