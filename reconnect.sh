568587cf9d17957fa777586f1db4e41bbed8db9c
Greg Stamper <gastamper@gmail.com>
2021-12-23 20:26:13 -0500

    Improvements to reconnect.sh

--- reconnect.sh
+++ reconnect.sh
@@ -1,17 +1,22 @@
 # This script cycles through Mullvad VPN endpoints, keeping track of used
 # endpoints in $HOME/mullvad.used.
 
+usedlist=$HOME/mullvad.used
 relaylist=$(mullvad relay list | grep wireguard | cut -f1 -d ' ' | tr -d '\t')
-used=$(cat $HOME/mullvad.used)
+
+if [[ -z $usedlist ]]; then
+	used=$(cat $HOME/mullvad.used)
+else
+	used=""
+fi
 
 for item in $relaylist; do
-	if [[ $used =~ (^|[[:space:]])$item($|[[:space:]]) ]] ; then	
+	if [[ $usedlist =~ (^|[[:space:]])$item($|[[:space:]]) ]] ; then	
 		echo "Already used $item"
 	else
 		echo "$item free"
-		echo "$item" >> $HOME/mullvad.used
-		mullvad relay set hostname $item
-		break
+		echo "$item" >> $usedlist
+		mullvad relay set hostname $item && break || echo "Setting relay failed"
 	fi
 	echo "All endpoints already used, exiting"
 done
