f1dd17b76cd3a2f2bbb48d9f0666338c9217a38f
Greg Stamper <gastamper@gmail.com>
2021-12-24 23:01:24 -0500

    Added randomize support, improved error handling

--- reconnect.sh
+++ reconnect.sh
@@ -2,7 +2,33 @@
 # endpoints in $HOME/mullvad.used.
 
 usedlist=$HOME/mullvad.used
-relaylist=$(mullvad relay list | grep wireguard | cut -f1 -d ' ' | tr -d '\t')
+if [[ ! -x "/usr/bin/mullvad" ]]; then
+	echo "/usr/bin/mullvad doesn't exist or isn't executable"
+	exit 1
+fi
+
+relays=$(mullvad relay list)
+retval=$?
+
+if [[ $? -ne 0 ]]; then
+	echo "Got error $retval running 'mullvad relay list'"
+	exit $retval
+fi
+
+relaylist=$(echo "$relays" | grep wireguard | cut -f1 -d ' ' | tr -d '\t')
+
+case $1 in
+	"--help")
+		echo "'$0 random' to randomize relay list"
+		exit 0
+		;;
+	"random")
+		relaylist=$(shuf -e $relaylist)
+		;;
+	*)
+		;;
+esac
+
 # Get list of countries by 2-character identifier
 # countries=$(mullvad relay list | sed '/^\t/d' | sed '/^$/d' | cut -f2 -d '(' | cut -f1 -d')' | tr '\n' '|')
 exclude="al|au|at|be|br|bg|cz|dk|ee|fi|fr|de|gr|hk|hu|ie|il|it|jp|lv|lu|md|nl|nz|mk|no|pl|pt|ro|rs|sg|es|se|ch|ae"
