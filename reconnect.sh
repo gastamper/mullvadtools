0e01d8e42e81592065964a827406f2bfe8ea3ed6
Greg Stamper <gastamper@gmail.com>
2021-12-25 09:59:36 -0500

    Cleaned up structure, moved check relay to function

--- reconnect.sh
+++ reconnect.sh
@@ -1,21 +1,31 @@
+#!/usr/bin/env bash
 # This script cycles through Mullvad VPN endpoints, keeping track of used
 # endpoints in $HOME/mullvad.used.
 
 usedlist=$HOME/mullvad.used
+cmd='echo "$relays" | grep wireguard | cut -f1 -d " " | tr -d "\t"'
+# Get list of countries by 2-character identifier
+# countries=$(mullvad relay list | sed '/^\t/d' | sed '/^$/d' | cut -f2 -d '(' | cut -f1 -d')' | tr '\n' '|')
+exclude="al|au|at|be|br|bg|cz|dk|ee|fi|fr|de|gr|hk|hu|ie|il|it|jp|lv|lu|md|nl|nz|mk|no|pl|pt|ro|rs|sg|es|se|ch|ae"
+
 if [[ ! -x "/usr/bin/mullvad" ]]; then
 	echo "/usr/bin/mullvad doesn't exist or isn't executable"
 	exit 1
 fi
 
-relays=$(mullvad relay list)
-retval=$?
+# Ensure 'mullvad relay list' command & argument works as expected
+check_relay() {
+	# Make $relays global
+	declare -gx relays=$(mullvad relay list)
+	retval=$?
 
-if [[ $? -ne 0 ]]; then
-	echo "Got error $retval running 'mullvad relay list'"
-	exit $retval
-fi
+	if [[ $? -ne 0 ]]; then
+		echo "Got error $retval running 'mullvad relay list'"
+		exit $retval
+	fi
+}
 
-relaylist=$(echo "$relays" | grep wireguard | cut -f1 -d ' ' | tr -d '\t')
+check_relay
 
 case $1 in
 	"--help")
@@ -23,20 +33,19 @@ case $1 in
 		exit 0
 		;;
 	"random")
-		relaylist=$(shuf -e $relaylist)
+		relaylist=$(eval $cmd | shuf)
 		;;
 	*)
+		relaylist=$(eval $cmd)
 		;;
 esac
 
-# Get list of countries by 2-character identifier
-# countries=$(mullvad relay list | sed '/^\t/d' | sed '/^$/d' | cut -f2 -d '(' | cut -f1 -d')' | tr '\n' '|')
-exclude="al|au|at|be|br|bg|cz|dk|ee|fi|fr|de|gr|hk|hu|ie|il|it|jp|lv|lu|md|nl|nz|mk|no|pl|pt|ro|rs|sg|es|se|ch|ae"
-
+# Create list of already used relays if it doesn't exist
 if [[ ! -f $usedlist ]]; then
 	touch $usedlist
 fi
 
+# Loop through relays, exclude any which don't match, connect to first valid
 for item in $relaylist; do
 	if [[ $(cat $usedlist) =~ (^|[[:space:]])$item($|[[:space:]]) ]] ; then	
 		echo "Already used $item"
