adc6be0b074400b52c1209dec4b9131856d56a1c
Greg Stamper <gastamper@gmail.com>
2021-12-26 10:31:07 -0500

    Rewrite of argument parsing

--- reconnect.sh
+++ reconnect.sh
@@ -6,6 +6,15 @@ usedlist=$HOME/mullvad.used
 cmd='echo "$relays" | grep wireguard | cut -f1 -d " " | tr -d "\t"'
 exclude="al|au|at|be|br|bg|cz|dk|ee|fi|fr|de|gr|hk|hu|ie|il|it|jp|lv|lu|md|nl|nz|mk|no|pl|pt|ro|rs|sg|es|se|ch|ae"
 
+# Output usage info
+usage() { 
+	# Auto-generate documentation based on comments
+	echo "$0 usage:" && { grep ".) \#" $0 | 
+	tr -d "\t" | cut -f2 -d"|"; }; 
+	exit 0;
+}
+
+
 # Check if mullvad binary in expected location and executable
 check_executable() {
 	if [[ ! -x "/usr/bin/mullvad" ]]; then
@@ -24,33 +33,44 @@ check_relay() {
 	fi
 }
 
-# Parse command-line arguments
-parse_args() {
-	case $1 in
-		"--help")
-			echo "'$0 random' to randomize relay list"
-			echo "'$0 countries' to get a list of countries"
-			exit 0
-			;;
-		"random")
-			relaylist=$(eval $cmd | shuf)
-			;;
-		"countries")
-			mullvad relay list | sed '/^\t/d' | sed '/^$/d' | cut -f2 -d '(' | cut -f1 -d')' | tr '\n' '|'
-			echo -e \r\n
-			exit 0
-			;;
-		*)
-			relaylist=$(eval $cmd)
-			;;
-	esac
+# Parse command line arguments
+check_args() {
+	local OPTIND
+	while getopts ":rxhc" flag
+	do
+		case $flag in
+			\?)
+				echo "Invalid option: -$OPTARG" >&2
+				exit 1;;
+			r) # Randomize relay list
+				export -n random=1;;
+			x) # Do not exclude previously used relays
+				exclude=1;;
+			c) # List countries with available relays, then exit
+				mullvad relay list | sed '/^\t/d' | sed '/^$/d' | cut -f2 -d '(' | cut -f1 -d')' | tr '\n' '|'
+				echo -e \r\n
+				exit 0
+				;;
+			h) # Display help
+				usage
+				exit 0;;
+			*) ;;
+		esac
+	done
 }
 
 main() {
+	# If requiring flags is needed later
+	# [[ $# -eq 0 ]] && usage
+	check_args $1
 	check_executable
 	check_relay
-	parse_args $1
-	
+
+	# Randomize relay list if -r flag not passed
+	[[ $random -ne 1 ]] && 
+		relaylist=$(eval $cmd | shuf) ||
+		relaylist=$(eval $cmd);
+
 	# Create list of already used relays if it doesn't exist
 	[[ ! -f $usedlist ]] &&	touch $usedlist
 
@@ -73,4 +93,4 @@ main() {
 	exit 1
 }
 
-main $1
+main "$@"
